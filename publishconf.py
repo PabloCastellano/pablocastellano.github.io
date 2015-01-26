0e95087a29560ba28e37dffa5bd609b8da59bd5c
Pablo Castellano <pablo@anche.no>
2015-01-26 19:09:34 +0100

    publish SITEURL pablog.me

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://127.0.0.1:8000'
+SITEURL = 'http://pablog.me'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
