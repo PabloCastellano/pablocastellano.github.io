3247c95812518756f973510deff78ce27222f479
Pablo Castellano <pablo@anche.no>
2015-01-26 18:55:27 +0100

    fabfile marsupi-carmen

--- fabfile.py
+++ fabfile.py
@@ -10,8 +10,8 @@ env.deploy_path = 'output'
 DEPLOY_PATH = env.deploy_path
 
 # Remote server configuration
-production = 'root@localhost:22'
-dest_path = '/var/www'
+production = 'pablog@marsupi-carmen:22'
+dest_path = '/home/pablog/pablog.me'
 
 # Rackspace Cloud Files configuration settings
 env.cloudfiles_username = 'my_rackspace_username'
